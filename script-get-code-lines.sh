b249d4faf34f1ceb08c5e14171fdd1ab176ee5c4
Camilo0414 <juancamilo0414@hotmail.es>
2019-02-22 10:14:50 -0500

    commit a la master

--- script-get-code-lines.sh
+++ script-get-code-lines.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-git ls-files | xargs wc -l > logs/code_lines.log
+#
+git ls-files | xargs wc -l > log/code_lines.log
--- script-get-commits.sh
+++ script-get-commits.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-git log --branches --not --remotes=*/master --numstat --date=format:'%Y-%m-%d %H:%M:%S'  --pretty=format:'%h-%ad-%aN' > logs/script.log
+typeset branches
+typeset after
+typeset before
+typeset commits
 
+branches=`git branch -a | nawk -F/ '$1~/remotes/ && $2~/origin/ && $3!~/HEAD/ && $3!~/master/ { print $3} {if($4!="" && $3!~/HEAD/) print "/" $4}'`
+
+echo -e "Please, type the date since when you want your commit history. /nFormat YYYY-MM-DD HH:MM:SS/n"
+read after
+echo -e "Please, type the date until when you want your commit history. /nFormat YYYY-MM-DD HH:MM:SS/n"
+read before
+
+for br in $branches;
+do
+#	git checkout $b | git log --branches
+#commits=``
+echo $branches
